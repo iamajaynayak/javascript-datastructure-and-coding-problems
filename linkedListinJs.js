ce91ff6f0a7436748a067dae28220e9de643ca8c
Ajay Nayak <63513168+iamajaynayak@users.noreply.github.com>
2021-01-11 14:15:20 +0530

    creating Linked list in Js
    How to create a singly Linked list in JavaScript

---
+++ linkedListinJs.js
@@ -0,0 +1,76 @@
+// Question : Create a Linked list in JavaScript 
+// singly linked list
+
+class LinkedList {
+	constructor(value){
+		this.head = {
+			value : value,
+			next : null
+		}
+		this.tail = this.head;  // With one node, the head and the tail is same node
+		this.length = 1; // length is 1, so index will start from 1
+	}
+ /* append(value) to add a new value/Node to the end of the list */
+	append(value){
+		const newNode = {
+			value : value,
+			next : null
+		}
+		this.tail.next = newNode;
+		this.tail = newNode;
+		this.length++
+		return this
+	}
+ /* prepend(value) to add a new value/Node at the start of the list */
+	prepend(value){
+		const newNode ={
+			value : value,
+			next : null
+		}
+		newNode.next = this.head
+		this.head = newNode
+		this.length++
+		return this
+	}
+ /* traverse(index) to find a node at a specific index {index starts at 1 here, since this.length is equal to 1 }  */
+	traverse(index){
+		let counter = 0;
+		let currentNode = this.head
+		while(counter !== index){
+			currentNode = currentNode.next
+			counter++
+		}
+		return currentNode
+	}
+ /* insertion(index, value) to insert a value/Node at a specific index */
+	insertion(index , value){
+		const newNode ={
+			value : value,
+			next : null
+		}
+
+		let findPrevIndex = this.traverse(index-1)
+		let temp = findPrevIndex.next
+		findPrevIndex.next = newNode;
+		newNode.next = temp;
+		return this
+	}
+
+ /* printList() to print the linked list*/
+	printList(){
+		let arr = []
+		let currentNode = this.head;
+		while(currentNode !== null){
+			arr.push(currentNode.value)
+			currentNode = currentNode.next
+		}
+		return arr;
+	}
+}
+
+const myLinkedList = new LinkedList(20) // initial value
+myLinkedList.append(30);
+myLinkedList.append(50)
+myLinkedList.prepend(10);
+myLinkedList.insertion(3,40)
+myLinkedList.printList()
