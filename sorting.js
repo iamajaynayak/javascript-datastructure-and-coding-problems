caea1208d4031cb1b1dcb1be068376f5460434da
Ajay Nayak <63513168+iamajaynayak@users.noreply.github.com>
2021-01-17 11:33:35 +0530

    bubble sort, selection sort
    Added selection sort

--- sorting.js
+++ sorting.js
@@ -3,8 +3,8 @@
 function bubbleSort(arr){
 	let leng = arr.length;
 	let temp;
-	for(let i=0; i<=leng; i++){
-		for(let j=0; j<=leng; j++){
+	for(let i=0; i<leng; i++){
+		for(let j=0; j<leng; j++){
 			if(arr[j] > arr[j+1]){
 				// Swapping values if 2nd value is grater than 1st value
 				temp = arr[j];
@@ -16,3 +16,26 @@ function bubbleSort(arr){
 	return arr
 }
 bubbleSort([5,6,3,4,7,4,2,1,1,5,4,9])
+
+// Selection sort
+
+function selectionSort(arr){
+	let leng = arr.leng;
+	let min;
+	for(let i = 0; i < leng; i++){
+		min = i
+		for(let j= i+1; j < leng; j++){
+			if(arr[j] < arr[min]){
+				min = j
+			}
+		}
+		if(i != min){
+			let temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp
+		}
+	}
+	return arr
+}
+
+selectionSort([7,4,5,9,8,2,1])
